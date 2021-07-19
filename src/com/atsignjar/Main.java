249e925ddb5313e921235c6b5507c044a5ad4707
Jeremy Parker <jeremyeparker@yahoo.com>
2021-07-19 17:01:52 -0400

    game loops until first lose

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -1,5 +1,6 @@
 package com.atsignjar;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,36 +13,43 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Die die1 = new Die();
         Die die2 = new Die();
-        die1.roll();
-        die2.roll();
-        int total = die1.value + die2.value;
 
-        System.out.println(die1.value + " + " + die2.value + " = " + (total) );
-        System.out.println("Will the next number be (h)igher or (l)ower?");
-        String input = scanner.next();
 
-        die1.roll();
-        die2.roll();
-        int newTotal = die1.value + die2.value;
-        if (total == newTotal) {
-            System.out.println("same number you lose");
+        while(true){
+            die1.roll();
+            die2.roll();
+            int total = die1.value + die2.value;
+            System.out.println(die1.value + " + " + die2.value + " = " + (total) );
+            System.out.println("Will the next number be (h)igher or (l)ower?");
+            String input = scanner.next();
+
+            die1.roll();
+            die2.roll();
+            int newTotal = die1.value + die2.value;
+            if (total == newTotal) {
+                System.out.println("same number you lose");
+            }
+
+            boolean isLarger = newTotal > total;
+            System.out.println(die1.value + " + " + die2.value + " = " + (newTotal) );
+            System.out.println(isLarger ? "Number is higher" : "Number is lower");
+            boolean didWin = false;
+            if (input.equals("h")) { //guessed higher
+                if (isLarger) {
+                    didWin = true;
+                }
+            } else { //guessed lower
+                if (!isLarger) {
+                    didWin = true;
+                }
+            }
+            System.out.println(didWin ? "you WIN" : "You LOSE");
+            System.out.println("------------");
+            if(!didWin){
+                break;
+            }
+
         }
 
-        boolean isLarger = newTotal > total;
-        System.out.println(die1.value + " + " + die2.value + " = " + (newTotal) );
-        System.out.println(isLarger ? "Number is higher" : "Number is lower");
-        boolean didWin = false;
-        if (input.equals("h")) { //guessed higher
-            if (isLarger) {
-                didWin = true;
-            }
-        } else { //guessed lower
-            if (!isLarger) {
-                didWin = false;
-            }
-        }
-        System.out.println(didWin ? "you WIN" : "You LOSE");
-
-        scanner.close();
     }
 }

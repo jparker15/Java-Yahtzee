2ef7cfa8c0b38d857ce758d3d7d40ca34a5e66b4
Jeremy Parker <jeremyeparker@yahoo.com>
2021-07-19 16:40:03 -0400

    higher/lower dice game

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -1,26 +1,47 @@
 package com.atsignjar;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        Die die = new Die ();
-        Random rand = new Random();
-        Cup cup = new Cup (5);
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        Die die1 = new Die();
+        Die die2 = new Die();
+        die1.roll();
+        die2.roll();
+        int total = die1.value + die2.value;
 
+        System.out.println(die1.value + " + " + die2.value + " = " + (total) );
+        System.out.println("Will the next number be (h)igher or (l)ower?");
+        String input = scanner.next();
 
+        die1.roll();
+        die2.roll();
+        int newTotal = die1.value + die2.value;
+        if (total == newTotal) {
+            System.out.println("same number you lose");
+        }
 
-        Input.welcome();
-        Input.demo();
-//        cup.roll(rand);
-//        System.out.println(cup);
-////        cup.roll(rand, Input.getChoices());
-////        System.out.println(cup);
-//
-//       cup.roll(rand, Input.getChoices());
-//        System.out.println(cup);
+        boolean isLarger = newTotal > total;
+        System.out.println(die1.value + " + " + die2.value + " = " + (newTotal) );
+        System.out.println(isLarger ? "Number is higher" : "Number is lower");
+        boolean didWin = false;
+        if (input.equals("h")) { //guessed higher
+            if (isLarger) {
+                didWin = true;
+            }
+        } else { //guessed lower
+            if (!isLarger) {
+                didWin = false;
+            }
+        }
+        System.out.println(didWin ? "you WIN" : "You LOSE");
+
+        scanner.close();
     }
 }
